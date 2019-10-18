0e29559f9b5dc1036df70bcbf12d7ef017fcb9e6
Jose Zuniga <zunigajose21@gmail.com>
2019-10-18 14:39:10 -0600

    Creacion del switch y del ArrayList

--- Lab2_JoseZuniga__31841432/src/lab2_josezuniga__31841432/Lab2_JoseZuniga__31841432.java
+++ Lab2_JoseZuniga__31841432/src/lab2_josezuniga__31841432/Lab2_JoseZuniga__31841432.java
@@ -1,21 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2_josezuniga__31841432;
 
-/**
- *
- * @author josez
- */
-public class Lab2_JoseZuniga__31841432 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Lab2_JoseZuniga__31841432 {
+    static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Universidad> lista = new ArrayList<>();
+        int op = 15;
+        boolean ingresado = false;
+        while (op != 9) {
+            op = menu();
+            switch (op) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    System.out.print("Ingrese su nombre de usuario: ");
+                    String user = leer.nextLine();
+                    System.out.print("Ingrese su Contraseña: ");
+                    String pass = leer.nextLine();
+                    if (user.equals("leobanegas") && pass.equals("99")) {
+                        System.out.println("\nIngresado Correctamente!");
+                        ingresado = true;
+                    } else {
+                        System.out.println("Error! ingreso incorrecto!!!");
+                    }
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                case 8:
+                    
+                    break;
+                case 9:
+                    
+                    break;
+            }
+        }
     }
+    
+    public static int menu() {
+        System.out.print("1. Crear Universidades\n"
+                + "2. Eliminar Universidades\n"
+                + "3. Log In\n"
+                + "4. Ascender Universidad\n"
+                + "5. Listar Universidades y su Sucursal\n"
+                + "6. Modificar Universidad\n"
+                + "7. Descender Universidad\n"
+                + "8. RANDOM\n"
+                + "9. Salir: ");
+        int op = leer.nextInt();
+        leer.nextLine();
+        if (op < 1 || op > 9) {
+            return menu();
+        }
+        return op;
+    }
+    
     
 }
--- Lab2_JoseZuniga__31841432/src/lab2_josezuniga__31841432/Universidad.java
+++ Lab2_JoseZuniga__31841432/src/lab2_josezuniga__31841432/Universidad.java
@@ -1,14 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2_josezuniga__31841432;
 
-/**
- *
- * @author josez
- */
+import java.util.Date;
+
 public class Universidad {
-    
+    private String nombre, nomRector, sucursal, nivel;
+    private Date year;
+    private int numMaestros, numEstudiantes, costo;
+
+    public Universidad() {
+    }
+
+    public Universidad(String nombre, String nomRector, String sucursal, String nivel, Date year, int numMaestros, int numEstudiantes, int costo) {
+        this.nombre = nombre;
+        this.nomRector = nomRector;
+        this.sucursal = sucursal;
+        this.nivel = nivel;
+        this.year = year;
+        this.numMaestros = numMaestros;
+        this.numEstudiantes = numEstudiantes;
+        this.costo = costo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNomRector(String nomRector) {
+        this.nomRector = nomRector;
+    }
+
+    public void setSucursal(String sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public void setNumMaestros(int numMaestros) {
+        this.numMaestros = numMaestros;
+    }
+
+    public void setNumEstudiantes(int numEstudiantes) {
+        this.numEstudiantes = numEstudiantes;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNomRector() {
+        return nomRector;
+    }
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public int getNumMaestros() {
+        return numMaestros;
+    }
+
+    public int getNumEstudiantes() {
+        return numEstudiantes;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+       
 }

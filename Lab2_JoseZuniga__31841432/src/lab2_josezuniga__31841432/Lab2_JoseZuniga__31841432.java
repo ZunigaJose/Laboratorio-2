7cd3edd57c4f4ce4c2fa54278431e1d1b266e3de
Jose Zuniga <zunigajose21@gmail.com>
2019-10-18 14:25:14 -0600

    Clase Universidad y menu

---
+++ Lab2_JoseZuniga__31841432/src/lab2_josezuniga__31841432/Lab2_JoseZuniga__31841432.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_josezuniga__31841432;
+
+/**
+ *
+ * @author josez
+ */
+public class Lab2_JoseZuniga__31841432 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
